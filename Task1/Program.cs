777be2da610b3755f65309397cfc6edeecac2dc2
Sure <schurovdaniil@gmail.com>
2024-02-10 17:49:42 +0700

    Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿string Charofstring(char[,] arr)
+{
+    string st = "";
+    foreach (var item in arr)
+    {
+        st += item;
+    }
+    return st;
+}
+
+char[,] chars = new char[,] 
+{
+    {'a', 'r', 't', 'y'},
+    {'e', 'g', 'p', 'f'},
+    {'n', 'c', 'q', 'x'}
+};
+Console.WriteLine(Charofstring(chars));
