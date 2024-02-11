75a9935003f0181ac253c010bd4d2236606488ad
Sure <schurovdaniil@gmail.com>
2024-02-11 14:07:49 +0700

    Task3

---
+++ Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿string palicheck (string sample)
+{
+    string result = ("");
+    for (int i = 0; i < sample.Length/2; i++)
+    {
+        if(sample[i] == sample[(sample.Length-1)-i])
+        {
+            result =  ("Образец - палиндром");
+        }
+        else
+        {
+            result =  ("Образец - не палиндром");
+        }
+    }
+    return result;
+}
+
+Console.Write("Введите образец: ");
+string str = Console.ReadLine();
+Console.WriteLine(palicheck(str));
